31f9b44f5c40e0998718f9e854d583f1974e96d9
cemkoc <cemkoc95@gmail.com>
2015-01-15 22:58:10 -0800

    added agent notification

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from twilio import twiml
+from twilio.rest import TwilioRestClient
 import os
 
 app = Flask(__name__)
@@ -14,8 +15,12 @@ def caller():
 @app.route('/wait', methods=['POST'])
 def wait():
 	response = twiml.Response()
-	response.say("Thank you for calling little grinch.")
+	response.say("Thank you for calling the grinch hotline.")
 	response.say("You are number %s in the queue." % request.form['QueuePosition'])
+	client = TwilioRestClient(ACda7d5ef08c17d20a529a663cde08e21b, 
+		ACda7d5ef08c17d20a529a663cde08e21b)
+	client.sms.message.create(to="+17770000", from="+14152983952", 
+		body="Hey someone is in the queue. Call to help !")
 	return str(response)
 
 @app.route('/agent', methods['POST'])
@@ -24,7 +29,7 @@ def agent():
 	with response.dial() as dial:
 		dial.queue("Christmas Call Queue")
 	return str(response)
-	
+
 
 
 if __name__ == "__main__":
